b2bbc5886a2ab52d4a6e7579ec18cd47a5ece57b
ILekoto <ishmael.lekoto@gmail.com>
2020-11-19 10:46:10 +0200

    first commit

---
+++ Login.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockSystems
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //check username and password
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-BVU2UB5\\ISHSQL;Initial Catalog=Stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT * FROM [Stock].[dbo].[Login] where userName = '"+ textBox1.Text+" ' and password = '"+ textBox2.Text +"'",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username and password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1_Click(sender, e);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockSystems
+{
+    public partial class Products : Form
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+
+        private void Product_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0; 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Products_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-BVU2UB5\\ISHSQL;Initial Catalog=Stock;Integrated Security=True");
+            //Insert logic
+            con.Open();
+            bool status = false;
+            if (comboBox1.SelectedIndex == 0)
+            {
+                status = true;
+            }
+            else {
+                status = false;
+            }
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[Products]
+           ([productCode]
+           ,[productName]
+           ,[productStatus])
+            VALUES
+           ('" + textBox1.Text +"','" + textBox2.Text +"','" + status +"')", con);
+           cmd.ExecuteNonQuery();
+           con.Close();
+
+           //reading data
+           SqlDataAdapter sda = new SqlDataAdapter(" select * from [stock].[dbo].[Products]", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = item["productCode"].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item["productName"].ToString();
+                if ((bool)item["productStatus"])
+                {
+                    dataGridView1.Rows[n].Cells[2].Value = "Active";
+                }
+                else
+                {
+                    dataGridView1.Rows[n].Cells[2].Value = "Deactive";
+                }
+                
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
